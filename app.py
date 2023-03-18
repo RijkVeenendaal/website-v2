886dc7ed38b980b70322e243099a25920b541a7e
RijkVeenendaal <128147800+RijkVeenendaal@users.noreply.github.com>
2023-03-18 16:53:54 +0000

    A lot

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
+href="{{ url_for('static', filename='images/plaatje.png') }}"
+
 app = Flask(__name__)
 
 @app.route("/")
 def Hello_World():
-  return render_template('home.html')
+  return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
